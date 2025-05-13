9b00390c5a99068b4055163af68f6cd67b9e51e3
nirmal <decembererror404@gmail.com>
2025-05-13 16:38:23 +0530

    add java basics

---
+++ Main.java
@@ -0,0 +1,37 @@
+package learnjava;
+// Ladder Satement Conditions
+
+// public class Main{
+//     public static void main(String[]args){
+//         int totalbusSeat=5;
+
+//         if(totalbusSeat<=20 && totalbusSeat>15){
+//             System.out.println("this is ordinary seat");
+//         }
+//         else if(totalbusSeat<=15 && totalbusSeat>10){
+//             System.out.println("this is disable people");
+//         }
+//         else if(totalbusSeat<=10 && totalbusSeat>5){
+//             System.out.println("this is family seat");
+//         }
+//         else if(totalbusSeat<=5 && totalbusSeat>=1){
+//             System.out.println("this is old people seat");
+//         }
+//         else{
+//             System.out.println("this is invalid!");
+//         }
+//     }
+// }
+
+
+public class Main{
+     public static void main(String[]args)
+     {
+          for(int i=1;i<=5;i++){
+          for(int j=1;j<=i;j++){
+               System.out.print("**");
+          } 
+          System.out.println();   
+          }
+     }
+}
