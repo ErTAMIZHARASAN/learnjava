001ea0a40725c99447dee99b8f1648645aae842b
nirmal <decembererror404@gmail.com>
2025-05-13 16:54:51 +0530

    add file

--- Main.java
+++ Main.java
@@ -24,6 +24,17 @@ package learnjava;
 // }
 
 
+// public class Main{
+//      public static void main(String[]args)
+//      {
+//           for(int i=1;i<=5;i++){
+//           for(int j=1;j<=i;j++){
+//                System.out.print("**");
+//           } 
+//           System.out.println();   
+//           }
+//      }
+// }
 public class Main{
      public static void main(String[]args)
      {
