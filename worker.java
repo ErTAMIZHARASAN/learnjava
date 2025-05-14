d8c2aa93ab230c9c418d3fa8153921257d5cc4b6
nirmal <decembererror404@gmail.com>
2025-05-14 18:29:29 +0530

    just add

---
+++ worker.java
@@ -0,0 +1,23 @@
+package learnjava;
+
+public class worker {
+    String empName;
+    int empId;
+    String empRole;
+
+    public worker(String empName,int empId,String empRole) {
+
+        this.empName=empName;
+        this.empId=empId;
+        this.empRole=empRole;
+
+    }
+    public void printDetails(){
+        System.out.print("Student Details: ");
+        SySystem.out.print()stem.out.println(this.empName+','+this.empId+","+this.empRolle);
+    }
+
+    
+
+    
+}
